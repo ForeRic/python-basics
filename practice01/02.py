6f64265e55dc518fdebd5566a777e53bf5785423
bitacademy-python <50367366+bitacademy-python@users.noreply.github.com>
2021-04-02 17:18:05 +0900

    Update 02.py

--- practice01/02.py
+++ practice01/02.py
@@ -1,5 +1,10 @@
+
 # 키보드로 정수 수치를 입력 받아 짝수인지 홀수 인지 판별하는 코드를 작성하세요.
-while True:
-    number = input('>')
-    number = int(number)
-    print(number)
+import sys
+
+number = input('수를 입력하세요: ')
+if number.isdigit() is False:
+    print('정수를 입력하세요')
+    sys.exit(0)
+
+print('짝수' if int(number) & 0x0001 == 0 else '홀수')
